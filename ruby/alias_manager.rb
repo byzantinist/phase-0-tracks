bdddfbf2c7d77d69ce8f794d22e8dbd0e2252c74
Eric Chiang <echiang777@yahoo.com>
2017-05-22 17:51:55 -0500

    Complete Release 1, create user interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,21 +1,29 @@
 # Welcome screen
 puts "Welcome to the DBC Alias Manager!"
+puts" You can continue entering names until you enter 'quit'"
 
-# Obtains spy's real name
-puts "What is the spy's real name which you need to disguise?"
-real_name = gets.chomp
+# While loop that is broken if the user enters "quit"
+while true
+	# Obtains spy's real name
+	puts "What is the spy's real name which you need to disguise?"
+	real_name = gets.chomp
 
-# Splits into an array
-disguise = real_name.split(' ')
+	if real_name.downcase == "quit"
+	break
+	end
 
-# Swaps the first and last name
-disguise = disguise.reverse
+	# Splits into an array
+	disguise = real_name.split(' ')
 
-# Changes vowels and consonents
-disguise.each do |x|
+	# Swaps the first and last name
+	disguise = disguise.reverse
+
+	# Changes vowels and consonents
+	disguise.each do |x|
 	x.tr!('aeiou', 'eioua')
 	x.tr!('AEIOU', 'EIOUA')
 	x.tr!('bcdfghjklmnpqrstvwxyz', 'cdfghjklmnpqrstvwxyzb')
 	x.tr!('BCDFGHJKLMNPQRSTVWXYZ', 'CDFGHJKLMNPQRSTVWXYZB')
+	end
+	puts disguise
 end
-puts disguise
