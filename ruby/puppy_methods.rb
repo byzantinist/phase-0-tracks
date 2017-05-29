6441c8eeaa2361bda818cf8e1b66676d3f80cf51
Eric Chiang <echiang777@yahoo.com>
2017-05-29 15:12:29 -0500

    Add behavior to the puppy for Release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,33 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+  	# Testing a different type of count increment
+  	count = ""
+  	while count.length < integer
+  		puts "Woof!"
+  		count += "z"
+  	end
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+  	dog_conversion = 7 * human_years
+	dog_conversion
+  end
+
+  def makes_a_puddle
+  	puts "Oop! There was no fire hydrant nearby so I made a puddle!"
+  end
+
 end
 
+kitty = Puppy.new
+kitty.fetch("ball")
+kitty.speak(5)
+kitty.roll_over
+puts kitty.dog_years(5)
+kitty.makes_a_puddle
