664455e7bc93a7e27302c026a8606f80880b9991
Eric Chiang <echiang777@yahoo.com>
2017-05-29 11:27:56 -0500

    Pseudocode Puppy class for Module 6-1

---
+++ ruby/puppy.rb
@@ -0,0 +1,27 @@
+# SPECIES -------------------------
+# Puppius Adorableus
+
+# CHARACTERISTICS -----------------
+# Name: varies
+# Age: 1 week
+# Gender: Male
+# Breed: Shiba Inu
+# Cuteness (1-10): 10
+# Friendliness (1-10): 9
+# Energy Level (1-10) 7
+# Intelligence (1-10): 3
+# GetS Along with Cats: Yes
+
+# BEHAVIOR-------------------------
+# Licks
+# Barks
+# Whines
+# Urinates
+# Defecates
+# Bites
+# Tears Things Up
+# Sits
+# Plays Fetch
+# Plays Dead
+# Rolls Over
+# Shakes Paws
