9b58e6d05eaa052a329718a5f3d8d8973b06cb04
Eric Chiang <echiang777@yahoo.com>
2017-05-29 19:52:03 -0500

    Initialize Santa attributes for Release 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,11 @@
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 
 	def speak
@@ -12,9 +16,26 @@ class Santa
 		puts "That was a good #{cookie}!"
 	end
 
+	def about
+		puts @gender
+		puts @ethnicity
+		puts @reindeer_ranking
+		puts @age
+	end
+
 end
 
 # Driver code
-ho1 = Santa.new
+ho1 = Santa.new("Baby Santa", "Indeterminate")
 ho1.speak
 ho1.eat_milk_and_cookies("oatmeal cookie")
+## ho1.about
+
+# Santa arrays
+santas = []
+santa_genders = ["male", "female", "agender", "intersex", "N/A"]
+santa_ethnicities = ["Caucasian", "Latina", "Asian", "Unicorn", "N/A"]
+santa_genders.length.times do |i|
+	santas << Santa.new(santa_genders[i], santa_genders[i])
+end
+puts santas
