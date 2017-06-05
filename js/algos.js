a9884c5e1da3c99911269e8b13dbe2fcff0d6d2a
Eric Chiang <echiang777@yahoo.com>
2017-06-05 00:13:26 -0500

    Complete key-value match for Release 1

--- js/algos.js
+++ js/algos.js
@@ -18,3 +18,40 @@ function longest(array) {
 
 // Driver code
 longest(["long phrase","longest phrase","longer phrase"])
+
+// Release 1: Find a Key-Value Match
+
+// 1. Read in two objects
+function key_value_match(object1, object2) {
+	key1 = Object.keys(object1);
+	key2 = Object.keys(object2);
+	value1 = key1.map(function(key) {
+		return object1[key];
+	})
+	value2 = key2.map(function(key) {
+		return object2[key];
+	})
+
+	match = false;
+
+	// 2. Use for loop for first object
+	for (var i = 0; i < key1.length; i++) {
+		// 3. Use for loop for second object
+		for (var j = 0; j < key2.length; j++) {
+			// 4. Look for match
+			if (key1[i] == key2[j]) {
+				if (value1[i] == value2[j]) {
+					match = true;
+				}
+			}
+		}
+	}
+	// 5. Return true or false
+	console.log(match);
+}
+
+// Driver code
+key_value_match({name: "Steven", age: 54}, {name: "Tamir", age: 54});
+key_value_match({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3});
+key_value_match({animal: "Dog", legs: 4}, {animal: "Cat", legs: 3});
+key_value_match({animal: "Dog", legs: 4}, {animal: "Cat", legs: 4});
